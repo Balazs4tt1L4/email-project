0da46545a4147798368b7b22c4ea59c89bbedeed
balazsa <balazsa@kkszki.hu>
2025-11-21 11:05:52 +0100

    menü

--- email project/Program.cs
+++ email project/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
+            //menüvezérlés
+            while (true)
+            {
+                Console.WriteLine("0. Kilépés");
+                Console.WriteLine("1. e-mailek listázása");
+                Console.WriteLine("2. új email");
+                Console.WriteLine("3. e-mail törlés");
+                Console.WriteLine("4. érvényes e-mailek listázása");
 
+                string valsztas = Console.ReadLine();
+                switch (valsztas)
+                {
+                    case "0": return;
+                    case "1": Console.WriteLine("e-mailek listázása"); break;
+                    case "2": Console.WriteLine("új e-mailek"); break;
+                    case "3": Console.WriteLine("e-mail törlés"); break;
+                    case "4": Console.WriteLine("érvényes e-mailek listázása"); break;
+                    default: Console.Clear(); Console.WriteLine("Rossz parancs"); break;
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
